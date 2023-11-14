def0b0c9f908c668c999b65ab61b35a119370c2a
bart.alcorn <bart.alcorn@turner.com>
2023-11-14 17:14:03 -0500

    updated acquire module

--- web/acquire/handlers.go
+++ web/acquire/handlers.go
@@ -55,16 +55,35 @@ func BriefDetails(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("error getting all", err)
 	}
 
-	t, err := template.ParseFiles("web/acquire/tmpls/acquireBriefDetails.gohtml", "web/acquire/tmpls/statusicons.gohtml")
-	if err != nil {
-		fmt.Println("error parsing gohtml", err)
-		w.WriteHeader(http.StatusInternalServerError)
+	isHTMX := r.Header.Get("HX-Request")
+	if isHTMX == "true" {
+		t, err := template.ParseFiles("web/acquire/tmpls/acquireBriefDetails.gohtml", "web/acquire/tmpls/statusicons.gohtml", "web/acquire/tmpls/discologo.gohtml")
+		if err != nil {
+			fmt.Println("error parsing gohtml", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		err = t.Execute(w, item)
+		if err != nil {
+			fmt.Println("error executing", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
 	}
-	err = t.Execute(w, item)
+
+	// jsn := fmt.Sprintf(
+	// 	"{\"status\": \"%s\", \"code\": \"%s\", \"detail\": \"%s\", \"date\": \"%s\"}",
+	// 	item.Asset.Status.Type,
+	// 	item.Asset.Status.Codes[0].Code,
+	// 	item.Asset.Status.Codes[0].Detail,
+	// 	item.Audit.CreatedAt,
+	// )
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	_, err = w.Write(nil)
 	if err != nil {
-		fmt.Println("error executing", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("failed to write", err)
 	}
+	return
 }
 
 // Read calls the GetAll func
@@ -84,6 +103,7 @@ func Read(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusAccepted)
 		_, err = w.Write(res)
 		if err != nil {
--- web/acquire/mongo.go
+++ web/acquire/mongo.go
@@ -7,14 +7,27 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+func SetTitle(a *Acquire) string {
+	result := "--No Title--"
+
+	for _, v := range a.Asset.Titles {
+		result = v.Full
+	}
+
+	return result
+}
+
 func Find(id string) (*Acquire, error) {
 	var result *Acquire
+	// item := make(map[string]interface{})
+
 	filter := bson.M{"id": id}
 	err := Acquires.FindOne(context.TODO(), filter).Decode(&result)
 	if err != nil {
 		log.Println("FindOne", id, err)
 	}
 
-	return result, err
+	result.Title = SetTitle(result)
 
+	return result, err
 }

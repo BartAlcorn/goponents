727286d7d502d1a6b9dd491eff65b682946620d7
bart.alcorn <bart.alcorn@turner.com>
2023-11-22 18:09:28 -0500

    refactored out Redis for now

--- pkg/application/app.go
+++ pkg/application/app.go
@@ -5,14 +5,12 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-
-	"github.com/redis/go-redis/v9"
 )
 
 type AppConfig struct {
-	router  http.Handler
-	RedisDB *redis.Client
-	config  Config
+	router http.Handler
+	Ctx    context.Context
+	config Config
 }
 
 var App *AppConfig
@@ -20,9 +18,6 @@ var App *AppConfig
 func init() {
 	config := LoadConfig()
 	App = &AppConfig{
-		RedisDB: redis.NewClient(&redis.Options{
-			Addr: config.RedisAddress,
-		}),
 		config: config,
 	}
 
@@ -30,29 +25,18 @@ func init() {
 }
 
 func (a *AppConfig) Start(ctx context.Context) error {
+	a.Ctx = ctx
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", a.config.ServerPort),
 		Handler: a.router,
 	}
 	fmt.Println("Starting on PORT", a.config.ServerPort)
-
-	err := a.RedisDB.Ping(ctx).Err()
-	if err != nil {
-		return fmt.Errorf("failed to connect to redis: %w", err)
-	}
-
-	defer func() {
-		if err := a.RedisDB.Close(); err != nil {
-			fmt.Println("failed to close redis", err)
-		}
-	}()
-
 	fmt.Println("Starting server")
 
 	ch := make(chan error, 1)
 
 	go func() {
-		err = server.ListenAndServe()
+		err := server.ListenAndServe()
 		if err != nil {
 			ch <- fmt.Errorf("failed to start server: %w", err)
 		}
@@ -60,7 +44,7 @@ func (a *AppConfig) Start(ctx context.Context) error {
 	}()
 
 	select {
-	case err = <-ch:
+	case err := <-ch:
 		return err
 	case <-ctx.Done():
 		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)

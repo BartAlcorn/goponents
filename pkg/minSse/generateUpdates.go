60e832028ac3bea45ac989995281b28cfa609806
bart.alcorn <bart.alcorn@turner.com>
2023-11-27 15:19:35 -0500

    removed stale code

--- pkg/minSse/generateUpdates.go
+++ pkg/minSse/generateUpdates.go
@@ -2,39 +2,10 @@ package minsse
 
 import (
 	"context"
-	"math/rand"
 	"time"
 
-	"github.com/bartalcorn/excuses"
 	"github.com/labstack/gommon/color"
 )
-
-func generateHistory(status string, a Asset) Asset {
-	l := len(a.History)
-	if l > 0 {
-		a.History[0].End = time.Now()
-	}
-	var h History
-	h.Process = tasks[rand.Intn(len(tasks))]
-	h.Status = status
-	h.Start = time.Now()
-	if status == "error" {
-		h.Description = excuses.Tech()
-	}
-	a.History = append([]History{h}, a.History...)
-	a = generateElapsed(a)
-	return a
-}
-
-func generateElapsed(a Asset) Asset {
-	l := len(a.History)
-	if l == 0 {
-		return a
-	}
-	a.History[1].End = time.Now()
-	a.History[1].Elapsed = a.History[1].End.Sub(a.History[1].Start).Round(3 * time.Millisecond)
-	return a
-}
 
 func removeAsset(i int) {
 	Assets = append(Assets[:i], Assets[i+1:]...)

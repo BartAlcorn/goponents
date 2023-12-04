c7e99f30ee89478902c84a23d981779bff565203
bart.alcorn <bart.alcorn@turner.com>
2023-12-04 07:58:46 -0500

    added theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "garden", "retro", "business", "aqua", "nord"],
-  }
-}
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "garden",
+      "retro",
+      "business",
+      "aqua",
+      "nord",
+      {
+        discolight: {
+          primary: "#208de2",
+          secondary: "#74B35E",
+          accent: "#0085FB",
+          neutral: "#0e100c",
+          "base-100": "#E6EFFE",
+          info: "#0086ff",
+          success: "#74B35E",
+          warning: "#FDBA5C",
+          error: "#D35A68",
+        },
+      },
+    ],
+  },
+};
